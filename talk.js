5f4a24a75a239fa2bd2af80f56cb425c30e08d84
nozakatomomi <nozaka@s14.yeskist.net>
2015-07-27 09:03:25 +0900

    プレゼンとか

--- talk.js
+++ talk.js
@@ -20,20 +20,20 @@ function speak() {
 			com='和食かぁ･･･。ちなみに肉と魚、どっちの気分？(※和の肉or和の魚)';
 				}else if
 				(player.match(/^(和の肉)$/i)){
-				com="それならこれらはどうかな？OK？";
+				com="それなら生姜焼きはどう？";
 				}else if
 				(player.match(/^(和の魚)$/i)){
-				com='それならこれらはどうかな？OK？';
+				com='それならブリの照り焼きはどう？';
 			
 		}else if
 			(player.match(/^(洋食|洋|洋風)$/i)){
 			com='洋風だね。ちなみに肉と魚、どっちの気分？(※洋の肉or洋の魚)';
 				}else if
 				(player.match(/^(洋の肉)$/i)){
-				com='それならこれらはどうかな？OK？';
+				com='それならチキンのトマト煮はどうかな？';
 				}else if
 				(player.match(/^(洋の魚)$/i)){
-				com='それならこれらはどうかな？OK？';
+				com='それなら鮭のムニエルはどうかな？';
 				
 		}else if
 		(player.match(/^(中華|中華料理|中)$/i)){
@@ -41,17 +41,21 @@ function speak() {
 			
 				}else if
 				(player.match(/^(中華の肉)$/i)){
-				com='それならこれらはどうかな？OK？';
+				com='それならバンバンジーはどうかな？';
 				}else if
 				(player.match(/^(中華の魚介)$/i)){
-				com='それならこれらはどうかな？OK？';
+				com='それならエビチリはどうかな？';
 				
 			}else if
 			(player.match(/^(嫌|だめ|NO|No|やだ)$/i)){
 			com='うーん･･･';
 		}else if
-		(player.match(/^(いいよ|OK|ok|おｋ)$/i)){
+		(player.match(/^(いいよ|OK|ok|おｋ|いいね)$/i)){
 			com='よかった！';
+			
+		}else if
+		(player.match(/^(どっちもいや|どっちも嫌|それ以外|どれもいや)$/i)){
+			com='困ったときはラーメンで決まり！';
 			
 		}else if
 		(player.match(/^(ありがとう|ありがとー|ありがと)$/i)){
@@ -63,7 +67,16 @@ function speak() {
 			
 		}else if
 		(player.match(/^(料理して|つくって|作って)$/i)){
-			com='料理は好きだけど作ってあげられないんだ･･･';
+			com='僕は作ってあげられないんだ･･･';
+		
+		}else if
+		(player.match(/^(つかれた|疲れた)$/i)){
+			com='おつかれさま！';
+		
+		}else if
+		(player.match(/^(眠たい|ねむたい|ねむい|眠い)$/i)){
+			com='おやすみなさい～';
+			
 		}else {
 			com='ん･･･？';
 		}
@@ -72,7 +85,7 @@ function speak() {
 		}
 		
 myImage = new Array(
-	"images/fun.png", "images/happy.png", "images/normal.png", "images/sad.png"
+	"images/fun.png", "images/happy.png", "images/normal.png"
 );
 function change() {
 	rnd = Math.floor(Math.random() * myImage.length);
